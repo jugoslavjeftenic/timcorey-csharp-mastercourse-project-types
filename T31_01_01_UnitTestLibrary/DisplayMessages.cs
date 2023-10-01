e8942365dcd46b8e5d6a66692a5849b45fcb924a
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-10-01 13:19:11 +0200

    UnitTestLibrary

---
+++ T31_01_01_UnitTestLibrary/DisplayMessages.cs
@@ -0,0 +1,29 @@
+﻿namespace T31_01_01_UnitTestLibrary
+{
+    public class DisplayMessages
+    {
+        public string Greetings(string firstName, int hourOfTheDay)
+        {
+            string retVal;
+
+            if (hourOfTheDay < 5)
+            {
+                retVal = $"Go to bed {firstName}.";
+            }
+            else if (hourOfTheDay < 12)
+            {
+                retVal = $"Good morning {firstName}.";
+            }
+            else if (hourOfTheDay < 18)
+            {
+                retVal = $"Good afternoon {firstName}.";
+            }
+            else
+            {
+                retVal = $"Good evning {firstName}.";
+            }
+
+            return retVal;
+        }
+    }
+}
