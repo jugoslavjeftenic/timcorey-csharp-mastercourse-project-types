61e842a8f2c7eb155cc8e18b11e0e6328e9eb1ea
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-10-06 10:16:13 +0200

    HomeworkMiniProjectLibrary

---
+++ T31_02_01_HomeworkMiniProjectLibrary/BasicMathOperations.cs
@@ -0,0 +1,16 @@
+﻿namespace T31_02_01_HomeworkMiniProjectLibrary
+{
+    // Build a Class Library (.NET Standard) with a class that does the basic math operations.
+    // Create unit tests for all methods.
+
+    public class BasicMathOperations
+    {
+        public static double Add(double a, double b) { return a + b; }
+
+        public static double Subtract(double a, double b) { return a - b; }
+
+        public static double Multiply(double a, double b) { return a * b; }
+
+        public static double Divide(double a, double b) { return a / b; }
+    }
+}
