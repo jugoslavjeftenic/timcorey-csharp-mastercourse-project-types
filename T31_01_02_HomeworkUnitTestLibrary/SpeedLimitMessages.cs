dabd69ecbc48b4a4e3810da1bcd9530e626a99f9
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-10-06 09:48:13 +0200

    HomeworkUnitTestLibrary

---
+++ T31_01_02_HomeworkUnitTestLibrary/SpeedLimitMessages.cs
@@ -0,0 +1,29 @@
+﻿namespace T31_01_02_HomeworkUnitTestLibrary
+{
+    public class SpeedLimitMessages
+    {
+        public static string SpeedLimit(int speedLimit)
+        {
+            string output = "";
+
+            if (speedLimit <= 30)
+            {
+                output = "You are passing through the school zone.";
+            }
+            else if (speedLimit <= 50)
+            {
+                output = "You are passing through the city zone.";
+            }
+            else if (speedLimit <= 80)
+            {
+                output = "You are traveling outside of the city zone.";
+            }
+            else if (speedLimit <= 130)
+            {
+                output = "You are traveling on the highway.";
+            }
+
+            return output;
+        }
+    }
+}
