b7fc0f6dbb0c8427914b0cabe04728db83e97360
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-10-06 19:42:02 +0200

    SolutionMiniProjectLibrary

---
+++ T31_02_02_SolutionMiniProjectLibrary/Calculations.cs
@@ -0,0 +1,32 @@
+﻿namespace T31_02_02_SolutionMiniProjectLibrary
+{
+    public class Calculations
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Substract(double x, double y)
+        {
+            return x - y;
+        }
+
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        public double Divide(double x, double y)
+        {
+            double output = 0;
+
+            if (y != 0)
+            {
+                output = x / y;
+            }
+
+            return output;
+        }
+    }
+}
