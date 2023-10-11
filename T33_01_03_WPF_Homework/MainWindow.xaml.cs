80bd80a07f94f7e8f9b41447e57b952cc5af1a93
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-10-11 19:40:11 +0200

    WPF_Homework

---
+++ T33_01_03_WPF_Homework/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace T33_01_03_WPF_Homework
+{
+    // Build a WPF application that has a simple data entry screen
+    // with First and Last name fields.
+    // Have a button say "Hi {FN} {LN}" when pressed.
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            firstNameTextBox.Focus();
+        }
+
+        private void SayHiButton_Click(object sender, RoutedEventArgs e)
+        {
+            sayHiLabel.Text = $"Hi {firstNameTextBox.Text} {lastNameTextBox.Text}";
+        }
+    }
+}
