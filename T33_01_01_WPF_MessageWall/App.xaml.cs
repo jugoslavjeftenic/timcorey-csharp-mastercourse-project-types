c3acc01bcaab2273bc4c50566966a62e36768156
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-10-11 11:13:23 +0200

    WPF_MessageWall

---
+++ T33_01_01_WPF_MessageWall/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace T33_01_01_WPF_MessageWall
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ T33_01_01_WPF_MessageWall/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace T33_01_01_WPF_MessageWall
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly BindingList<string> messages = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            messageList.ItemsSource = messages;
+        }
+
+        private void AddMessage_Click(object sender, RoutedEventArgs e)
+        {
+            messages.Add(messageText.Text);
+            messageText.Text = string.Empty;
+        }
+    }
+}
