fd444303558266abdc526ac0829b7c01aa30a97a
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-10-11 11:14:37 +0200

    WPF_NetFrameworkDemoUI

---
+++ T33_01_02_WPF_NetFrameworkDemoUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace T33_01_02_WPF_NetFrameworkDemoUI
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
