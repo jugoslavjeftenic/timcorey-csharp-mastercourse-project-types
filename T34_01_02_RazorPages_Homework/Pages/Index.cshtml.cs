a962cdb25d85d6fae20c1bdd3372c6b8e51b550e
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-10-15 12:07:29 +0200

    RazorPages_Homework

--- T34_01_02_RazorPages_Homework/Pages/Index.cshtml.cs
+++ T34_01_02_RazorPages_Homework/Pages/Index.cshtml.cs
@@ -1,10 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace T34_01_02_RazorPages_Homework.Pages
 {
 	public class IndexModel : PageModel
 	{
 		private readonly ILogger<IndexModel> _logger;
+
+		[BindProperty]
+		public string FirstName { get; set; } = "";
+		[BindProperty]
+		public string LastName { get; set; } = "";
+		[BindProperty]
+		public string Output { get; set; } = "";
 
 		public IndexModel(ILogger<IndexModel> logger)
 		{
@@ -13,7 +21,12 @@ namespace T34_01_02_RazorPages_Homework.Pages
 
 		public void OnGet()
 		{
+		}
 
+		public IActionResult OnPost()
+		{
+			Output = $"Hi {FirstName} {LastName}.";
+			return Page();
 		}
 	}
 }
